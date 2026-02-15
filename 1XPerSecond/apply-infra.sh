7111d357e0bb1ed73c055a26c6944efa0021a294
ernesto905 <ernesto@enriquez.dev>
2026-02-15 16:40:44 -0500

    Add kube-prometheus submodule for Prometheus and Grafana integration

--- 1XPerSecond/apply-infra.sh
+++ 1XPerSecond/apply-infra.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
-kubectl apply --server-side -f prometheus-operator.yml
-kubectl apply --filename=db.yml,petclinic.yml,prometheus.yml
+
+minikube kubectl -- apply --server-side -f kube-prometheus/manifests/setup
+until minikube kubectl -- get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+minikube kubectl -- apply -f kube-prometheus/manifests/
+minikube kubectl -- apply --filename=db.yml,petclinic.yml,prometheus.yml
--- 1XPerSecond/teardown-infra.sh
+++ 1XPerSecond/teardown-infra.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
-minikube kubectl -- delete -f prometheus-operator.yml
+
+minikube kubectl -- delete --ignore-not-found=true -f kube-prometheus/manifests/ -f kube-prometheus/manifests/setup
 minikube kubectl -- delete --filename=db.yml,petclinic.yml,prometheus.yml
