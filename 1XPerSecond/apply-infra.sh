39b077ad9743575e70998df1bd219f0fc7196a4d
ernesto905 <ernesto@enriquez.dev>
2026-02-16 10:50:10 -0500

    Add postgres exporter for scraping postgres /metrics endpoint

--- 1XPerSecond/apply-infra.sh
+++ 1XPerSecond/apply-infra.sh
@@ -3,4 +3,4 @@
 minikube kubectl -- apply --server-side -f kube-prometheus/setup
 until minikube kubectl -- get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
 minikube kubectl -- apply -f kube-prometheus/
-minikube kubectl -- apply --filename=db.yml,petclinic.yml,prometheus.yml
+minikube kubectl -- apply --filename=db.yml,petclinic.yml,prometheus.yml,postgres-exporter.yml
--- 1XPerSecond/teardown-infra.sh
+++ 1XPerSecond/teardown-infra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
+minikube kubectl -- delete --filename=db.yml,petclinic.yml,prometheus.yml,postgres-exporter.yml
 minikube kubectl -- delete --ignore-not-found=true -f kube-prometheus/ -f kube-prometheus/setup
-minikube kubectl -- delete --filename=db.yml,petclinic.yml,prometheus.yml
