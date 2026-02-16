f8cefee9580bb408ec0605750c5484bf9328a71f
ernesto905 <ernesto@enriquez.dev>
2026-02-15 20:06:31 -0500

    Fix Submodules not showing up
    kube prometheus was not being included in the clone, so I removed the
    submodule and simply included the appropriate manifest files for the
    building the observability stack.

--- 1XPerSecond/apply-infra.sh
+++ 1XPerSecond/apply-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-minikube kubectl -- apply --server-side -f kube-prometheus/manifests/setup
+minikube kubectl -- apply --server-side -f kube-prometheus/setup
 until minikube kubectl -- get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
-minikube kubectl -- apply -f kube-prometheus/manifests/
+minikube kubectl -- apply -f kube-prometheus/
 minikube kubectl -- apply --filename=db.yml,petclinic.yml,prometheus.yml
--- 1XPerSecond/teardown-infra.sh
+++ 1XPerSecond/teardown-infra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-minikube kubectl -- delete --ignore-not-found=true -f kube-prometheus/manifests/ -f kube-prometheus/manifests/setup
+minikube kubectl -- delete --ignore-not-found=true -f kube-prometheus/ -f kube-prometheus/manifests/setup
 minikube kubectl -- delete --filename=db.yml,petclinic.yml,prometheus.yml
